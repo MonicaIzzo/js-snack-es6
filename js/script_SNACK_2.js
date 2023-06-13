c5593f974246f7616725fc4d52418542449d689d
MonicaIzzo <info@monicaizzo.net>
2023-06-13 15:04:39 +0200

    preliminary operations

---
+++ js/script_SNACK_2.js
@@ -0,0 +1,31 @@
+/* traccia 
+SNACK 2
+Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+Per preparare l'aula di un nuovo corso, dobbiamo svolgere una serie di operazioni
+1. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+2.Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+3.  dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo ES (Marco della Rovere => MARCO DELLA ROVERE);
+Questo è l'elenco degli studenti:
+Id  Name                Grades
+213 Marco della Rovere      78
+110 Paola Cortellessa       96
+250 Andrea Mantegna 	    48
+145 Gaia Borromini          74
+196 Luigi Grimaldello 	    68
+102 Piero della Francesca   50
+120 Francesca da Polenta    84 
+Consigli del giorno
+Leggiamo bene le tracce, tutte le informazioni sono qui :faccia_leggermente_sorridente:
+Cerchiamo di utilizzare i metodi che abbiamo visto stamattina
+Buon lavoro!
+*/
+
+console.log('JS OK')
+
+// #
+
+// #
+
+// #
+
+// #
