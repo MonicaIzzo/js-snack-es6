441260a2540b8320530fde15d35270ffa5a26ea8
MonicaIzzo <info@monicaizzo.net>
2023-06-18 01:30:46 +0200

    SNACK 2 create array

--- js/script_SNACK_2.js
+++ js/script_SNACK_2.js
@@ -14,15 +14,48 @@ Id  Name                Grades
 196 Luigi Grimaldello 	    68
 102 Piero della Francesca   50
 120 Francesca da Polenta    84 
-Consigli del giorno
-Leggiamo bene le tracce, tutte le informazioni sono qui :faccia_leggermente_sorridente:
-Cerchiamo di utilizzare i metodi che abbiamo visto stamattina
-Buon lavoro!
 */
 
 console.log('JS OK')
 
-// #
+// # creo l'arrey dei studenti
+const students = [
+    {
+        id: 213,
+        name: "Marco della Rovere",
+        grades: 78,
+    },
+    {
+        id: 110,
+        name: "Paola Cortellessa",
+        grades: 96,
+    },
+    {
+        id: 250,
+        name: "Andrea Mantegna ",
+        grades: 48,
+    },
+    {
+        id: 145,
+        name: "Gaia Borromini ",
+        grades: 74,
+    },
+    {
+        id: 196,
+        name: "Luigi Grimaldello ",
+        grades: 68,
+    },
+    {
+        id: 102,
+        name: "Piero della Francesca",
+        grades: 50,
+    },
+    {
+        id: 120,
+        name: "Francesca da Polenta",
+        grades: 84,
+    },
+];
 
 // #
 
